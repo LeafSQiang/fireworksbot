b6dea6b5a4c4f150da07ad7aa1cf6358fb9213f0
Simon Shi <wenxs@microsoft.com>
2018-02-07 16:12:24 +0800

    add template

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=fireworksbot-Web-Deploy.pubxml -p:Password=oEgi60ZdKTD4f0TWG5D7Py43KWpDD7oRx0QKvNiBwbusHw1MracdqkW7PqgW
+
